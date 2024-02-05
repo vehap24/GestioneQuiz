d5ffac5d9469682f76c1493a905acd0ab4f39ced
vehap24 <bvehap46@gmail.com>
2024-02-05 17:29:38 +0100

    ciao andre come stai

--- gioca.php
+++ gioca.php
@@ -9,8 +9,31 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </head>
 <body>
-    <div class="text-center">
-       <img style="height: 7%; width: 7%" src="logo.jpg"> 
+    <div class="text-center container">
+        <img style="height: 7%; width: 7%" src="logo.jpg"> 
+        <form id="FORM" action="GET">
+            <div class="row">
+                <div class="col">
+                    <input class="btn-check" type="radio" id="1" name="risposta" value="1">
+                    <label class="btn btn-outline-secondary" for="1">HTML</label>
+                </div>
+                <div class="col">
+                    <input class="btn-check" type="radio" id="2" name="risposta" value="2"> 
+                    <label class="btn btn-outline-secondary" for="2">HTML</label>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col">
+                    <input class="btn-check" type="radio" id="3" name="risposta" value="3">
+                    <label class="btn btn-outline-secondary" for="3">HTML</label>
+                </div>
+                <div class="col">
+                    <input class="btn-check" type="radio" id="4" name="risposta" value="4">
+                    <label class="btn btn-outline-secondary" for="4">HTML</label>
+                </div>
+            </div>
+        </form>
     </div>
+    
 </body>
 </html>

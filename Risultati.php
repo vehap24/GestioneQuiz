50fc5eab48ca96c90179b2f7306750c6a6a168c6
AndreNatarov <andrenatarovmauri@gmail.com>
2024-02-07 09:32:37 +0100

    Messi placeholder

--- Risultati.php
+++ Risultati.php
@@ -32,66 +32,87 @@ echo "/20 risposte corrette";
 echo "<br>";
 switch($risposte_corrette){
 case 0:
+    echo "<br>COME DIAVOLO HAI FATTO?!?!?!<br>";
     echo "<img src='immagine_1.jpg'>";
     break;
 case 1:
+    "<br>Frase 2<br>";
     echo "<img src='immagine_2.jpg'>";
     break;
 case 2:
+    "<br>Frase 3<br>";
     echo "<img src='immagine_3.jpg'>";
     break;
 case 3:
+    "<br>Frase 4<br>";
     echo "<img src='immagine_4.jpg'>";
     break;
 case 4:
+    "<br>Frase 5<br>";
     echo "<img src='immagine_5.jpg'>";
     break;
 case 5:
+    "<br>Frase 6<br>";
     echo "<img src='immagine_6.jpg'>";
     break;
 case 6:
+    "<br>Frase 7<br>";
     echo "<img src='immagine_7.jpg'>";
     break;
 case 7:
+    "<br>Frase 8<br>";
     echo "<img src='immagine_8.jpg'>";
     break;
 case 8:
+    "<br>Frase 9<br>";
     echo "<img src='immagine_9.jpg'>";
     break;
 case 9:
+    "<br>Frase 10<br>";
     echo "<img src='immagine_10.jpg'>";
     break;
 case 10:
+    "<br>Frase 11<br>";
     echo "<img src='immagine_11.jpg'>";
     break;
 case 11:
+    "<br>Frase 12<br>";
     echo "<img src='immagine_12.jpg'>";
     break;
 case 12:
+    "<br>Frase 13<br>";
     echo "<img src='immagine_13.jpg'>";
     break;
 case 13:
+    "<br>Frase 14<br>";
     echo "<img src='immagine_14.jpg'>";
     break;
 case 14:
+    "<br>Frase 15<br>";
     echo "<img src='immagine_15.jpg'>";
     break;
 case 15:
+    "<br>Frase 16<br>";
     echo "<img src='immagine_16.jpg'>";
     break;
 case 16:
+    "<br>Frase 17<br>";
     echo "<img src='immagine_17.jpg'>";
     break;
 case 17:
+    "<br>Frase 18<br>";
     echo "<img src='immagine_18.jpg'>";
     break;
 case 18:
+    "<br>Frase 19<br>";
     echo "<img src='immagine_19.jpg'>";
     break;
 case 19:
+    "<br>Frase 20<br>";
     echo "<img src='immagine_20.jpg'>";
     break;
 case 20:
+    "<br>Frase 21<br>";
     echo "<img src='immagine_21.jpg'>";
     break;
 }
